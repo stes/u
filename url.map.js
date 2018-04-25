95a9268e5ffd696d20836a0553d6f86ac91b4d8d
stes <steffen.schneider@rwth-aachen.de>
2018-04-25 03:01:10 +0200

    Added link

--- url.map.js
+++ url.map.js
@@ -1,16 +1,5 @@
 (typeof window !== 'undefined' ? window : global).URLMap = {
-  "/undefined": "https://ant.design/",
-  "/": "https://ant.design/",
-  "/date-picker-value": "https://ant.design/components/date-picker/#components-date-picker-demo-format",
-  "/time-picker-value": "https://ant.design/components/time-picker/#components-time-picker-demo-size",
-  "/calendar-value": "https://ant.design/components/calendar/#components-calendar-demo-custom-render",
-  "/item-render": "https://ant.design/components/breadcrumb/#API",
-  "/menu-on-open-change": "https://ant.design/components/menu/#components-menu-demo-sider-current",
-  "/fixed-columns": "https://ant.design/components/table/#components-table-demo-fixed-columns",
-  "/table-row-key": "https://ant.design/components/table/#Note",
-  "/date-picker-on-open-change": "https://ant.design/components/date-picker/#components-date-picker-demo-start-end",
-  "/popover-content": "https://ant.design/components/popover/#components-popover-demo-basic",
-  "/get-field-decorator": "https://ant.design/components/form/#components-form-demo-horizontal-login",
-  "/gg2moment": "https://github.com/ant-design/ant-design/commit/4026221d451b246956983bb42140142d4a48b7d7",
-  "/codesandbox-repro": "https://codesandbox.io/api/v1/sandboxes/define?parameters=N4IgZglgNgpgziAXKADgQwMYGs0HMYB0AVnAPYB2SoGFALjObVSACYwoNvkYTzMBOMTE0QgoaenCYAaEIOEBaFqQC2SMRPgyQaRi3XjJ2laoYiQAJhABfW7Ijk2AD2IJkIGozPqIKlKX5aAAIAJSEMYLB-VSCAcnkI2IBuAB1yX39A0PDaABEAeQBZIKiY-JylVWS0jIDgkxUzEuiVOIazavS_OqDgINzNAAUIbBh-aSCANzG4CAog62ay3VoWTtqs2JWWAHoWCCkd7YIMODhOtLDhAsKCQUcxgAo0oKCAHn3JoKkAT1gAXmAfRUaH4uAciCCFgALAtrAA-F6vd4ob60P4wQHA0Hg8gAIVItFoqkhMLhiPIyORAGEAK78e7BbZTGZzciQ4DTfizCiLN4AI34OwpVNeAE1SLSghhdNKABa6fBBZlcnmU-a0OUwIKwMDBdDkGBQIKPXLsTgMHjwb4wCJsgCUBCRrzeOxQIuRbwG9GGo34QTYYDQtKgtAAamgoLTMcB2oxHvbFsLkUjXZ94dIkcoMLTGowCPhaABRWB52h4n4ASRYj3ipEJsXtaXtSRs9geLjltBUUGYnnojHUHwgXwgLH-KTk9dok_haZH8JstiAA",
+  "/undefined": "http://stes.io/",
+  "/": "http://stes.io/",
+  "/f7WM8kh": "https://www.google.de"
 };
